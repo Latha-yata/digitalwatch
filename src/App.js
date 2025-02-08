b78ffdf8d0a8843724938ee11991d1acec9d276e
Latha-yata <lathayata1234@gmail.com>
2025-02-08 17:55:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DigitalWatch from './digitalwatch/Digitalwatch';
+
+function App() {
+  return (
+    <div className="App">
+      <DigitalWatch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/digitalwatch/Digitalwatch.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function DigitalWatch() {
+  const [state, setState] = useState({
+    currenttime: new Date().toLocaleTimeString() 
+    // Initialize with current time
+  });
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setState({
+        currenttime: new Date().toLocaleTimeString()
+      });
+    }, 1000);
+    
+    return () => {
+      clearInterval(interval); // Clean up the interval on unmount
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        backgroundColor: 'green',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+        
+      }}
+    >
+
+      <div className="container m-0 mx-5 "  >
+
+        <div className="row">
+          <div className="col-md-4">
+          <div className="card border shadow-lg card-hover">
+
+              <div className="card-header bg-warning text-center">
+                <p className="h2 p-3 ">Digital Watch</p>
+              </div>
+              <div className="card-body bg-light text-center p-5 card-body-hover">
+                <h1>{state.currenttime}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DigitalWatch;
